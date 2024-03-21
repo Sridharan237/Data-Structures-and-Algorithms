0c9c8acd6dc378089e25e9134b8b9caa339bebd3
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-03-21 19:40:03 +0530

    Added linkedlist create and display program using c language

---
+++ 11.Linked List/Iterative Display Linked List/linkedlistcreateanddisplay.c
@@ -0,0 +1,54 @@
+//C program to create and display linked list 
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node * next;
+}*first=NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+
+    int i;
+
+    first = (struct Node*)malloc(sizeof(struct Node));       //first (or) head node
+
+    first->data = A[0];                     
+    first->next = NULL;
+    last = first;
+
+    for(i=1;i<n;i++)       //create and append to the previous node and make point the last pointer to the created node
+    {
+        t = (struct Node*)malloc(sizeof(struct Node));
+
+        t->data = A[i];
+        t->next = NULL;
+
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node* p)
+{
+    while(p != NULL)
+    {
+        printf("%d->", p->data);
+        p = p->next;
+    }
+    printf("NULL");
+}
+
+int main()
+{
+    int A[] = {3, 5, 7, 9, 10};
+
+    create(A, 5);
+
+    Display(first);
+
+    return 0;
+}
