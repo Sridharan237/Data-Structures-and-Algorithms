f04d9d5bbc35f3c90bc26f64d5a1e2cb05848abd
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-03-23 23:06:54 +0530

    Added counting no. of nodes in a linked list

---
+++ 11.Linked List/count in linked list/Recursivecount.c
@@ -0,0 +1,80 @@
+//C program to count no. of nodes in linked list recursively
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node * next;
+}*first=NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+
+    int i;
+
+    first = (struct Node*)malloc(sizeof(struct Node));       //first (or) head node
+
+    first->data = A[0];                     
+    first->next = NULL;
+    last = first;
+
+    for(i=1;i<n;i++)       //create and append to the previous node and make point the last pointer to the created node
+    {
+        t = (struct Node*)malloc(sizeof(struct Node));
+
+        t->data = A[i];
+        t->next = NULL;
+
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node* p)
+{
+    while(p != NULL)
+    {
+        printf("%d->", p->data);
+        p = p->next;
+    }
+    printf("NULL\n");
+}
+
+int Iterativecount(struct Node* p)
+{
+    int count = 0;
+
+    while(p)
+    {
+        count++;
+        p = p->next;
+    }
+
+    return count;
+}
+
+int RecursiveCount(struct Node* p)
+{
+    if(p)
+    {
+        return RecursiveCount(p->next) + 1;
+    }
+
+    return 0;
+}
+
+
+int main()
+{
+    int A[] = {3, 5, 7, 9, 10};
+
+    create(A, 5);
+
+    Display(first);
+
+    printf("No. of nodes is : %d\n", RecursiveCount(first));
+    
+    return 0;
+}
