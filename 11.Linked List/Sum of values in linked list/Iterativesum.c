3064ec7377b544ec604fa2a4b8cf25c448595126
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-03-23 23:07:50 +0530

    Added adding of all the values in a linked list

---
+++ 11.Linked List/Sum of values in linked list/Iterativesum.c
@@ -0,0 +1,83 @@
+//C program to sum of all elements in linked list iteratively
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node * next;
+}*first=NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+
+    int i;
+
+    first = (struct Node*)malloc(sizeof(struct Node));       //first (or) head node
+
+    first->data = A[0];                     
+    first->next = NULL;
+    last = first;
+
+    for(i=1;i<n;i++)       //create and append to the previous node and make point the last pointer to the created node
+    {
+        t = (struct Node*)malloc(sizeof(struct Node));
+
+        t->data = A[i];
+        t->next = NULL;
+
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node* p)
+{
+    while(p != NULL)
+    {
+        printf("%d->", p->data);
+        p = p->next;
+    }
+    printf("NULL\n");
+}
+
+int IterativeCount(struct Node* p)
+{
+    int count = 0;
+
+    while(p)
+    {
+        count++;
+        p = p->next;
+    }
+
+    return count;
+}
+
+int IterativeSum(struct Node* p)
+{
+    int sum = 0;
+
+    while(p)
+    {
+        sum += p->data;
+        p = p->next;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int A[] = {3, 5, 7, 9, 10};
+
+    create(A, 5);
+
+    Display(first);
+
+    printf("Sum of all values in linked list is : %d\n", IterativeSum(first));
+
+
+    return 0;
+}
