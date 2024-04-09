cbe7f7e80fa5caad1ac0ccb17ee57aa39e758bad
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-04-09 18:15:59 +0530

    Added Doubly Linked List create, display, finding length using c

---
+++ 11.Linked List/Doubly Linked List/Create and Display DLL/createanddisplaydll.c
@@ -0,0 +1,76 @@
+// c program to create, display and find the length of a doubly linked list
+#include<stdio.h>
+#include<stdlib.h>
+
+// Node structure of a doubly linked list
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+// function for creating a doubly linked list
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    int i;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+
+    first->prev = NULL;
+    first->data = A[0];
+    first->next = NULL;
+
+    last = first;
+
+    for(i=1;i<n;i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+
+        t->data = A[i];
+        t->next = NULL;
+
+        last->next = t;
+        t->prev = last;
+        last = t;
+    }
+}
+
+// function to find the length of a doubly linked list
+int Length(struct Node *p)
+{
+    int len = 0;
+
+    while(p)
+    {
+        len++;
+        p = p->next;
+    }
+
+    return len;
+}
+
+// function to display a doubly linked list
+void Display(struct Node *p)
+{
+    while(p)
+    {
+        printf("%d<->", p->data);
+        p = p->next;
+    }
+    printf("NULL");
+}
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5};
+
+    create(A, 5);
+
+    printf("\nLength : %d\n", Length(first));
+
+    Display(first);
+
+    return 0;
+}
