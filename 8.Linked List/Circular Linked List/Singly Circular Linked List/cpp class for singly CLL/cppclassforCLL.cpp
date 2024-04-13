2a429bcc4137a6902883b0577cfae4e330b3347d
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-04-13 15:31:32 +0530

    Added some changes to cpp class for singly Circular Linked List in cpp

--- 8.Linked List/Circular Linked List/Singly Circular Linked List/cpp class for singly CLL/cppclassforCLL.cpp
+++ 8.Linked List/Circular Linked List/Singly Circular Linked List/cpp class for singly CLL/cppclassforCLL.cpp
@@ -20,6 +20,9 @@ public:
     Node* getHead() {return head;}
     void Display();
     void RecursiveDisplay(Node *p);
+    int Length();
+    void Insert(int index, int x);
+    int Delete(int index);
 };
 
 // implementing the functions of the above class CircularLinkedList
@@ -93,11 +96,117 @@ void CircularLinkedList :: RecursiveDisplay(Node *p)
     flag = 0;
 }
 
+int CircularLinkedList :: Length()
+{
+    int len = 0;
+
+    Node *p = head;
+
+    while(p->next != head)
+    {
+        len++;
+        p = p->next;
+    }
+
+    return len;
+}
+
+void CircularLinkedList :: Insert(int index, int x)
+{
+    int i;
+
+    Node *p = head;
+
+    if(index < 0 || index > Length())
+        return ;
+
+    Node *t;
+
+    t = new Node;
+
+    t->data = x;
+    t->next = NULL;
+
+    if(index == 0)
+    {
+        if(head == NULL)
+        {
+            head = t;
+            head->next = head;
+        }
+        else
+        {
+            while(p->next != head)
+                p = p->next;
+
+            t->next = head;
+            p->next = t;
+            head = t;
+        }
+    }
+    else 
+    {
+        for(i=0;i<index-1;i++)
+            p = p->next;
+        
+        t->next = p->next;
+        
+        p->next = t;
+    }
+}
+
+int CircularLinkedList :: Delete(int index)
+{
+    int i, x = -1; // x - deleted element 
+
+    Node *p, *q;
+    p = head, q = NULL;
+
+    if(index < 1 || index > Length())
+        return -1;
+
+    if(index == 1)
+    {
+        x = head->data;
+
+        while(p->next == head)
+            p = p->next;
+
+        if(p == head)
+        {
+            delete head;
+            head = nullptr;
+        }
+        else
+        {
+            p->next = head->next;
+            delete head;
+            head = p->next;
+        }
+    }
+    else
+    {
+        for(i=0;i<index-2;i++)
+            p = p->next;
+        
+        q = p->next;
+        p->next = q->next;
+
+        x = q->data;
+
+        delete q;
+    }
+
+    return x;
+}
+
 int main()
 {
     int A[] = {1, 2, 3, 4, 5};
 
     CircularLinkedList cl(A, sizeof(A)/sizeof(A[0]));
+
+    cl.Insert(0, 10);
 
     cl.Display();
 }
